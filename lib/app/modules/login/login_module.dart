108611d496f52f5f3907734387a5d1b52797bbda
Toshi Ossada <toshiossada@gmail.com>
2021-03-10 15:54:36 -0300

    correcao

--- lib/app/modules/login/login_module.dart
+++ lib/app/modules/login/login_module.dart
@@ -3,6 +3,7 @@ import 'package:dio/native_imp.dart';
 import 'domain/repositories/authenticate_repository_interface.dart';
 import 'domain/usecases/authenticate_by_login.dart';
 import 'domain/usecases/interfaces/authenticate_by_login_interface.dart';
+import 'infra/data/datasource/authenticate_datasource_interface.dart';
 import 'infra/data/repositories/authenticate_repository.dart';
 import 'infra/external/datasource/authenticate_datasource.dart';
 import 'presenter/pages/login/login_controller.dart';
@@ -13,9 +14,10 @@ import 'presenter/pages/login/login_page.dart';
 class LoginModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => LoginController(i<IAuthenticateByLogin>())), 
+        Bind((i) => LoginController(i<IAuthenticateByLogin>())),
         Bind((i) => AuthenticateDatasource(i<DioForNative>())),
-        Bind((i) => $AuthenticateRepository),
+        Bind<IAuthenticateRepository>(
+            (i) => AuthenticateRepository(i<IAuthenticateDatasource>())),
         Bind((i) => AuthenticateByLogin(i<IAuthenticateRepository>())),
       ];
 
